39d5439d0c84cba0345c347a95aaf3c8438914bf
Devon <devonsteenberg@hotmail.com>
2019-01-17 08:24:56 +1300

    Added clojure project

---
+++ clojure/code-advent-2018/test/code_advent_2018/core_test.clj
@@ -0,0 +1,7 @@
+(ns code-advent-2018.core-test
+  (:require [clojure.test :refer :all]
+            [code-advent-2018.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
