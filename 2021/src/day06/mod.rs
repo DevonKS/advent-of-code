c75b32e42961255240a560fe6cec17ccb24fab3c
Devon Steenberg <devonsteenberg@hotmail.com>
2021-12-07 20:40:04 +1300

    2021 Day 6 refactor

--- 2021/src/day06/mod.rs
+++ 2021/src/day06/mod.rs
@@ -1,7 +1,5 @@
 use crate::utils;
 
-use std::collections::HashMap;
-
 pub fn run(it: utils::InputType) {
     let input = read_input(it);
     println!("{}", part1(&input));
@@ -9,47 +7,35 @@ pub fn run(it: utils::InputType) {
 }
 
 fn part2(fish: &Vec<u8>) -> u64 {
-    let mut fish_map: HashMap<u8, u64> = HashMap::new();
-    fish_map.insert(0, 0);
-    fish_map.insert(1, 0);
-    fish_map.insert(2, 0);
-    fish_map.insert(3, 0);
-    fish_map.insert(4, 0);
-    fish_map.insert(5, 0);
-    fish_map.insert(6, 0);
-    fish_map.insert(7, 0);
-    fish_map.insert(8, 0);
-
-    for f in fish {
-        *fish_map.get_mut(f).unwrap() += 1
-    }
-
-    for _i in 0..256 {
-        let num_new = fish_map[&0];
-        for i in 0..8 {
-            *fish_map.get_mut(&i).unwrap() = fish_map[&(i + 1)];
-        }
-
-        *fish_map.get_mut(&6).unwrap() += num_new;
-        *fish_map.get_mut(&8).unwrap() = num_new;
-    }
-    fish_map.values().sum()
+    simulate_growth(256, fish)
+        .iter()
+        .sum::<u64>()
+        .try_into()
+        .unwrap()
 }
 
-fn part1(fish: &Vec<u8>) -> u32 {
-    let mut local_fish = fish.clone();
-    for _i in 0..80 {
-        for i in 0..local_fish.len() {
-            let f = local_fish[i];
-            if f == 0 {
-                local_fish[i] = 6;
-                local_fish.push(8);
-            } else {
-                local_fish[i] -= 1;
-            }
-        }
+fn part1(fish: &Vec<u8>) -> u64 {
+    simulate_growth(80, fish)
+        .iter()
+        .sum::<u64>()
+        .try_into()
+        .unwrap()
+}
+
+fn simulate_growth(iterations: u32, fish: &Vec<u8>) -> Vec<u64> {
+    let mut fish_pop = [0; 9];
+
+    for f in fish {
+        fish_pop[*f as usize] += 1
     }
-    local_fish.len().try_into().unwrap()
+
+    for _i in 0..iterations {
+        let num_new = fish_pop[0];
+        fish_pop.rotate_left(1);
+        fish_pop[6] += num_new;
+        fish_pop[8] = num_new;
+    }
+    fish_pop.to_vec()
 }
 
 fn read_input(it: utils::InputType) -> Vec<u8> {
